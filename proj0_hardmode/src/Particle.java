604480a87a40b6ed31e61abf8f4109ee81659585
quettalum <1972270176@qq.com>
2026-05-14 23:28:09 +0800

    Task 1-3.

--- proj0_hardmode/src/Particle.java
+++ proj0_hardmode/src/Particle.java
@@ -1,2 +1,40 @@
+import java.util.Map;
+import java.awt.Color;
+
 public class Particle {
+    public static final int PLANT_LIFESPAN = 150;
+    public static final int FLOWER_LIFESPAN = 75;
+    public static final int FIRE_LIFESPAN = 10;
+    public static final Map<ParticleFlavor, Integer> LIFESPANS =
+            Map.of(ParticleFlavor.FLOWER, FLOWER_LIFESPAN,
+                    ParticleFlavor.PLANT, PLANT_LIFESPAN,
+                    ParticleFlavor.FIRE, FIRE_LIFESPAN);
+
+    public static final Map<ParticleFlavor, Color> COLORS =
+            Map.of(ParticleFlavor.EMPTY, Color.BLACK,
+                    ParticleFlavor.SAND, Color.YELLOW,
+                    ParticleFlavor.BARRIER, Color.GRAY,
+                    ParticleFlavor.WATER, Color.BLUE,
+                    ParticleFlavor.FOUNTAIN, Color.CYAN,
+                    ParticleFlavor.PLANT, new Color(0, 255, 0),
+                    ParticleFlavor.FIRE, new Color(255, 0, 0),
+                    ParticleFlavor.FLOWER, new Color(255, 141, 161));
+
+    int lifespan;
+    ParticleFlavor flavor;
+
+    Particle(ParticleFlavor flavor)
+    {
+        this.flavor = flavor;
+        this.lifespan = -1;
+        if (LIFESPANS.get(this.flavor) != null)
+        {
+            this.lifespan = LIFESPANS.get(this.flavor);
+        }
+    }
+
+    public Color color()
+    {
+        return COLORS.get(this.flavor);
+    }
 }
--- proj0_hardmode/tests/TestParticle.java
+++ proj0_hardmode/tests/TestParticle.java
@@ -1,2 +1,36 @@
+import org.junit.jupiter.api.Test;
+import static com.google.common.truth.Truth.assertThat;
+import java.awt.Color;
+
 public class TestParticle {
+    @Test
+    public void testConstructor() {
+        Particle fp = new Particle(ParticleFlavor.FIRE);
+        assertThat(fp.flavor).isEqualTo(ParticleFlavor.FIRE);
+        assertThat(fp.lifespan).isEqualTo(10);
+
+        Particle sp = new Particle(ParticleFlavor.SAND);
+        assertThat(sp.flavor).isEqualTo(ParticleFlavor.SAND);
+        assertThat(sp.lifespan).isEqualTo(-1);
+    }
+
+    @Test
+    public void testColor() {
+        Particle emptyParticle = new Particle(ParticleFlavor.EMPTY);
+        assertThat(emptyParticle.color()).isEqualTo(Color.BLACK);
+        Particle sandParticle = new Particle(ParticleFlavor.SAND);
+        assertThat(sandParticle.color()).isEqualTo(Color.YELLOW);
+        Particle barrierParticle = new Particle(ParticleFlavor.BARRIER);
+        assertThat(barrierParticle.color()).isEqualTo(Color.GRAY);
+        Particle waterParticle = new Particle(ParticleFlavor.WATER);
+        assertThat(waterParticle.color()).isEqualTo(Color.BLUE);
+        Particle fountainParticle = new Particle(ParticleFlavor.FOUNTAIN);
+        assertThat(fountainParticle.color()).isEqualTo(Color.CYAN);
+        Particle plantParticle = new Particle(ParticleFlavor.PLANT);
+        assertThat(plantParticle.color()).isEqualTo(new Color(0, 255, 0));
+        Particle fireParticle = new Particle(ParticleFlavor.FIRE);
+        assertThat(fireParticle.color()).isEqualTo(new Color(255, 0, 0));
+        Particle flowerParticle = new Particle(ParticleFlavor.FLOWER);
+        assertThat(flowerParticle.color()).isEqualTo(new Color(255, 141, 161));
+    }
 }
